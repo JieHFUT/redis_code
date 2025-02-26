907c57fa68dc8b30b5e24b5eaf153dce82e3b039
JieCHERY <9594523wrj@gmail.com>
2025-02-26 16:59:50 +0800

    redis-jedis

--- c-redis-cluster/src/main/java/com/jiehfut/redis/crediscluster/service/OrderService.java
+++ c-redis-cluster/src/main/java/com/jiehfut/redis/crediscluster/service/OrderService.java
@@ -30,7 +30,6 @@ public class OrderService {
         // 调用 API 来设置键值
         redisTemplate.opsForValue().set(key, value);
 
-
         log.info("========= key:{}, value:{} ==========", key, value);
     }
 
